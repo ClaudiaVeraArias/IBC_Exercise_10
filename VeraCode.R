2f47a406b181359b7239048c92384096df47ae6a
Claudia Abigail Vera <cvera@crcfe02.crc.nd.edu>
2019-12-07 17:31:05 -0500

    need more things

--- VeraCode.R
+++ VeraCode.R
@@ -7,135 +7,56 @@
 # rate in the absence of the drug and the non-mutant sub-population declines rapidly. 
 # The model we will use to represent the growth of the two sub-populations is this:
 
+ ## set the working directory
+setwd('/Users/claudiaveraarias/Documents/ND_Classes/Fall_Semester_2019/Biocomputing/R/W14_BC')
+
+    ## oppen packages
+library(ggplot2)
+
     ## 1) Parameters
-N0=0
-M0=0
-rN=0.1
-rM=0.1
-K=1000000
-m=1/10000
-time=50
-rTNM=-0.1 #grow rate of cells non-mutant with drug treatment
-rTMC=0.5 #mutant cells grow at the 50% of they capacity when the treatment is present
-
-    ## 2) equation 
-N0=0
-N=numeric(length=time)
-N[1]=N0
-
-M0=0
-M=numeric(length = time)
-M[1]=M0
-
-
-N(t+1)=N[t]+rN*N[t](1-((N[t]+M[t])/K))
-M(t+1)=M[t]+rM*M[t](1-((N[t]+M[t])/K))
-
-    
-#### may be
-
-Nc<-numeric(time)
-Mc<-numeric(time)
-Nc0<-0
-Mc0<-50
-Nc[1]<-Nc0
-Mc[1]<-Mc0
-K=1000000
-r=0.1
-time=50
-test<-function(M0=50,r=0.1,K=100,time=50){
-  Mc<-numeric(time)
-  Mc0<-50
-  Mc[1]<-Mc0
-   for(i in 1:(time-1)){
-Mc[i+1]<-Mc[i]+r*Mc[i]*(1-(999900+Mc[i])/K)+0.5*Mc[i]
-}
-return(Mc)
-}
-
-
-
-##fuction that works
-prueba3<-function(Nc0=50,r=0.1,K=1000000,time=1000){
-  Nc<-numeric(time)
-  Nc0<-50
-  Nc[1]<-Nc0
-  for(i in 1:(time-1)){
-    Nc[i+1]<-Nc[i]+r*Nc[i]*(1-(Nc[i]+100)/K)
-  }
-  return(Nc)
-}
-### 350
-
-
-
-### Don't work :(
-
-Test1<-function(Nc0=10000,Mc0=1,r=0.1,K=1000000,time=500){
-  Nc<-numeric(time)
-  Nc0<-10000
-  Nc[1]<-Nc0
-  Mc<-numeric(time)
-  Mc0<-1
-  Mc[1]<-Mc0
-  for(i in 1:(time-1)){
-    if(i<=100)
-    Mc[i+1]<-Mc[i]+r*Mc[i]*(1-(999900+Mc[i])/K)-Mc[i]*0.5
-  }else{
-    Nc[i+1]<-Nc[i]+r*Nc[i]*(1-(Nc[i]+100)/K)-Nc[i]*-0.1
-  }
-  return(Nc)
-  return(Mc)
-}
-
-
-
-
-#### other
-#initial data
-N0=50
+N0=1
 M0=1
-r=0.1
+r=0.2
 K=1000000
 time=350
+rNT=0.9
+rMT=0.5
+    ## 2) equation 
 Ns=numeric(length = time)
 Ns[1]=N0
 Ms=numeric(length = time)
 Ms[1]=M0
 
-rNT=-0.1
-
-#simulation
-for(i in 1:200){
-  Ns[i+1]=Ns[i]+r*Ns[i]*(1-(Ns[i]+Ms[i])/K)-Ns[i]*rNT
+Ns[i+1]=Ns[i]+r*Ns[i]*(1-(Ns[i]+Ms[i])/K)-Ns[i]*rNT
+Ms[i+1]=Ms[i]+r*Ms[i]*(1-(Ns[i]+Ms[i])/K)-Ms[i]*rMT
+Ns[i+1]=Ns[i]+r*Ns[i]*(1-(Ns[i]+Ms[i])/K)
+    
+    ## 3)Loop
+popsim1<-function(N0=1,M0=1,r=0.2,K=1000000,time=350,rNT=0.9,rMT=0.5){
+  Ns=numeric(length = time)
+  Ns[1]=N0
+  Ms=numeric(length = time)
+  Ms[1]=M0
+  for(i in 1:350){
+    if(i<=100){
+      Ms[i+1]=Ms[i]+r*Ms[i]*(1-(Ns[i]+Ms[i])/K)-Ms[i]*rMT
+    }else if(i>=999900)
+    Ns[i+1]=Ns[i]+r*Ns[i]*(1-(Ns[i]+Ms[i])/K)-Ns[i]*rNT
+  }else{
+    Ns[i+1]=Ns[i]+r*Ns[i]*(1-(Ns[i]+Ms[i])/K)
+  }
+  return(Ns)
+  return(Ms)
 }
 
-#plot
-sim<-data.frame(time=1:length(Ns),N=Ns)
-ggplot(data=sim,aes(x=time,y=N))+geom_line()+theme_classic()
+   ## 4)Data frame
 
-
-
-
-
-#function
-
-popsim<-function(N0=1,M0=1,r=0.2,K=1000000,time=350,rNT=-0.1){
-Ns=numeric(length = time)
-Ns[1]=N0
-Ms=numeric(length = time)
-Ms[1]=M0
-for(i in 1:350){
-  Ns[i+1]=Ns[i]+r*Ns[i]*(1-(Ns[i]+Ms[i])/K)-Ns[i]*rNT
-}
-return(Ns)
-return(Ms)
-}
-
-Tumor=data.frame(time=1:351)
-Tumor$Mutant<-popsim(rNT = 0.5)
-Tumor$NoMutant<-popsim()
-Tumor$NoCancer<-popsim(rNT = 0)
+Tumor1=data.frame(time=1:351)
+Tumor1$Mutant<-popsim1(rNT = -0.9,rMT = -0.5)
+Tumor1$NoMutant<-popsim()
+Tumor1$NoCancer<-popsim1(rNT = 0,rMT = 0)
+  
+   ## 5)Plot 
 
 dim(Tumor)
 ggplot(data=Tumor)+
@@ -143,3 +64,6 @@ ggplot(data=Tumor)+
   geom_line(aes(x=time,y=NoMutant),col="pink")+
   geom_line(aes(x=time,y=NoCancer),col="purple")+
   theme_classic()
+
+
+
